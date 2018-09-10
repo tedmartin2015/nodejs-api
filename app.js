27932266ba5e42bb2bf38f469687b7cd0b4e2f42
Ted Martin <ted.martin.hermosisima@gmail.com>
2018-09-10 14:44:45 +0800

    added body-parser and stub for updateService

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || 3020;
 const mongoServer = process.env.MONGO_SERVER;
 const mongoDatabase = process.env.MONGO_DATABASE;
 
-const dbConfig = {
-    mongoServer,
-    mongoDatabase,
-    port
-};
+const dbConfig = { mongoServer, mongoDatabase, port };
 
 const adminRouter = require('./routes/adminRoutes') (dbConfig);
 const serviceRouter = require('./routes/serviceRoutes') (dbConfig);
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRouter);
 app.use('/services', serviceRouter);
--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -17,21 +17,26 @@ function serviceController (config) {
         } ());
     }
 
+    function updateService(req, res) {
+        (async function mongo() {
+
+        } ());
+    }
+
     function getAllServices(req, res) {
         (async function mongo() {
             let client;
             try {
-                client = await MongoClient.connect(config.mongoServer, { useNewUrlParser: true});
+                client = await MongoClient.connect(config.mongoServer, { useNewUrlParser: true });
                 const db = client.db(config.mongoDatabase);
                 const col = await db.collection('services');
                 const services = await col.find().toArray();
                 res.json(services);                
-                client.close()
+                client.close();
             }
             catch (err) {
                 console.log(err.stack);
             }
-
         } ());
     }
 
@@ -41,12 +46,19 @@ function serviceController (config) {
             let client;
             try { 
                 console.log(`SERVICEID: ${id}`);
-                client = await MongoClient.connect(config.mongoServer, { useNewUrlParser: true});
+                client = await MongoClient.connect(config.mongoServer, { useNewUrlParser: true });
                 const db = client.db(config.mongoDatabase);
                 const col = await db.collection('services');
-                const service = await col.findOne({ _id: new ObjectID(id)});
-                console.log(service)
-                res.json(service);
+                const service = await col.findOne({ _id: new ObjectID(id)}, (err, data) => {
+                    if (!err) {
+                        console.log(`SERVICE DETAILS: ${JSON.stringify(data)}`);
+                        res.json(data);
+                    }
+                    else {
+                        console.log(err.stack);
+                    }
+                });
+                               
                 client.close();
             }
             catch (err) {
@@ -59,7 +71,8 @@ function serviceController (config) {
         getAllServices,
         getServiceById,
         insertService,
-        deleteService
+        deleteService,
+        updateService
     };
 }
 
